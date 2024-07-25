b2ced7e35dc98313cb1e74b6433743352a733d8b
yulissa.jimenez@mifact.net <yulissa.jimenez@mifact.net>
2024-07-25 11:58:04 -0500

    creacion de proyecto angular y de componente cliente y listar cliente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListarClienteComponent } from './listar-cliente/listar-cliente/listar-cliente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListarClienteComponent
   ],
   imports: [
     BrowserModule,
