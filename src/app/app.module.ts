c537c5652657f2776e6ce2cf72db86af1b798cde
yulissa200 <julissa.jim20@gmail.com>
2024-07-25 13:04:08 -0500

    creacion de listar usuarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListarClienteComponent } from './listar-cliente/listar-cliente/listar-cliente.component';
 import { ListarProvedoresComponent } from './provedor/listar-provedores/listar-provedores.component';
+import { ListarUsuarioComponent } from './usuarios/listar-usuario/listar-usuario.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListarClienteComponent,
-    ListarProvedoresComponent
+    ListarProvedoresComponent,
+    ListarUsuarioComponent
   ],
   imports: [
     BrowserModule,
